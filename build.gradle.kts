bacb069a24579bd5da9075ba2e319f30c864591e
won <wondevc@gmail.com>
2024-02-22 00:52:44 +0900

    fix: mac os dns issue fix
    - Can not find io.netty.resolver.dns.macos.MacOSDnsServerAddressStreamProvider
    - https://github.com/netty/netty/issues/11020

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -25,6 +26,16 @@ subprojects {
 		dependencies {
 			implementation(`kotlin-reflect`)
 			implementation(`kotlin-stdlib`)
+
+			if (Os.isArch("x86_64")) {
+				if (Os.isFamily(Os.FAMILY_MAC)) {
+					runtimeOnly(`netty-dns-macos`(classifier = "osx-x86_64"))
+				}
+			} else if (Os.isArch("aarch64")) {
+				if (Os.isFamily(Os.FAMILY_MAC)) {
+					runtimeOnly(`netty-dns-macos`(classifier = "osx-aarch_64"))
+				}
+			}
 		}
 
 		tasks {
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,3 +7,8 @@ val `kotlin-stdlib`
 
 val `spring-boot-starter-web`
     get() = "org.springframework.boot:spring-boot-starter-web"
+
+fun `netty-dns-macos`(
+    version: String = Versions.NETTY_DNS_MACOS,
+    classifier: String? = null
+) = "io.netty:netty-resolver-dns-native-macos:${version}${if (classifier != null) ":${classifier}" else ""}"
