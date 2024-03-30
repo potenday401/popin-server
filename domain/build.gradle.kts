2c0bf4e436819764e6fdc62813b7db377058ee34
hei5enbug <kbm0598@gmail.com>
2024-03-31 01:35:58 +0900

    fix: jts-core 의존성 관리 buildSrc를 통해 하도록 변경

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 dependencies {
     implementation(`uuid-generator`())
-    implementation("org.locationtech.jts:jts-core:1.19.0")
+    implementation(`locationtech-jts-core`())
     jooqGenerator(`postgres-connector`())
 }
 
